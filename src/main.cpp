578418686cb52abd4e0e83069e76175c9fb4a1a8
Ariel Elbaum <ariel.elbaum@outlook.com>
2022-09-13 00:34:29 +1000

    Mechanical Done

--- src/main.cpp
+++ src/main.cpp
@@ -30,30 +30,150 @@ unsigned long currentTime = millis();
 unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
+// Speed can be 0-90, closer to zero the faster
+
+int threemin = 110;
+int onemin = 110/3l;
+
+void tick(int time, int speed)
+{
+  myservo.write(speed); //forward
+  delay(time); // time it is spinning
+  myservo.write(90); // stop
+}
 
 void C1()
 {
-  myservo.write(0);  // CW down shaft
-  delay(1000);       // delay 1 second
-  myservo.write(90); // stop
+  tick(threemin,20); // tick quickly into 9:30
+  delay(1100); // wait 1.1 second
+  tick(threemin,20); // tick quickly into 9:30
+  delay(1100); // wait 1.1 second
+  tick(threemin,20); // tick quickly into 9:33
+  delay(1100); // wait 1.1 second
+  tick(threemin,20); // tick quickly into 9:36
 }
 
 void C2()
 {
-  myservo.write(180); // CCW down shaft
-  delay(1000);        // delay 1 second
-  myservo.write(90);  // stop
+  tick(threemin,20); // tick quickly into 9:39
+  delay(1100); // wait 1.1 second
+  tick(threemin,20); // tick quickly into 9:42
+  delay(1100); // wait 1.1 second
+  tick(threemin,20); // tick quickly into 9:45
 }
 
 void C3()
 {
-  myservo.write(0);  // CW down shaft
-  delay(1000);       // delay 1 second
-  myservo.write(90); // stop
-  delay(1000);
-  myservo.write(180); // CCW down shaft
-  delay(1000);        // delay 1 second
-  myservo.write(90);  // stop
+  
+}
+
+void C4()
+{
+  tick(900000,80);
+}
+
+void C5()
+{
+  
+}
+
+void C6()
+{
+  
+}
+
+void C7()
+{
+  
+}
+
+void C8()
+{
+  
+}
+
+void C85()
+{
+  
+}
+
+void C9()
+{
+  
+}
+
+void C10()
+{
+  
+}
+
+void C11()
+{
+  
+}
+
+void C12()
+{
+  
+}
+
+void C13()
+{
+  for (int i = 0; i < 100; i++)
+  {
+    tick(threemin,0);
+    delay(1500);
+  }
+}
+
+void C14()
+{
+  tick(onemin,20);
+}
+
+void C15()
+{
+  tick(onemin,20);
+}
+
+void C16()
+{
+  tick(onemin,20);
+}
+
+void C17()
+{
+  tick(onemin,20);
+}
+
+void C18()
+{
+  tick(onemin,20);
+}
+
+void C19()
+{
+  tick(5000,80);
+}
+
+void C20()
+{
+  tick(300000,0);
+}
+
+void forward()
+{
+  tick(50,30);
+}
+
+void backward()
+{
+  tick(50,180);
+}
+
+void stop()
+{
+  myservo.write(90);
 }
 
 void setup()
@@ -115,16 +235,152 @@ void loop()
             {
               Serial.println("C1 Go");
               C1();
+              currentScene = "1";
             }
             if (header.indexOf("GET /C2/go") >= 0)
             {
               Serial.println("C2 Go");
               C2();
+              currentScene = "2";
             }
             if (header.indexOf("GET /C3/go") >= 0)
             {
               Serial.println("C3 Go");
               C3();
+              currentScene = "3";
+            }
+            if (header.indexOf("GET /C4/go") >= 0)
+            {
+              Serial.println("C4 Go");
+              C4();
+              currentScene = "4";
+            }
+            if (header.indexOf("GET /C5/go") >= 0)
+            {
+              Serial.println("C5 Go");
+              C5();
+              currentScene = "5";
+            }
+            if (header.indexOf("GET /C6/go") >= 0)
+            {
+              Serial.println("C6 Go");
+              C6();
+              currentScene = "6";
+            }
+            if (header.indexOf("GET /C7/go") >= 0)
+            {
+              Serial.println("7 Go");
+              C7();
+              currentScene = "7";
+            }
+            if (header.indexOf("GET /C8/go") >= 0)
+            {
+              Serial.println("C8 Go");
+              C8();
+              currentScene = "8";
+            }
+            if (header.indexOf("GET /C85/go") >= 0)
+            {
+              Serial.println("C8.5 Go");
+              C85();
+              currentScene = "8.5";
+            }
+            if (header.indexOf("GET /C9/go") >= 0)
+            {
+              Serial.println("C9 Go");
+              C9();
+              currentScene = "9";
+            }
+            if (header.indexOf("GET /C10/go") >= 0)
+            {
+              Serial.println("C10 Go");
+              C10();
+              currentScene = "10";
+            }
+            if (header.indexOf("GET /C11/go") >= 0)
+            {
+              Serial.println("C11 Go");
+              C11();
+              currentScene = "C11";
+            }
+            if (header.indexOf("GET /C12/go") >= 0)
+            {
+              Serial.println("C12 Go");
+              C12();
+              currentScene = "C12";
+            }
+            if (header.indexOf("GET /C13/go") >= 0)
+            {
+              Serial.println("C13 Go");
+              C13();
+              currentScene = "C13";
+            }
+            if (header.indexOf("GET /C14/go") >= 0)
+            {
+              Serial.println("C14 Go");
+              C14();
+              currentScene = "C14";
+            }
+            if (header.indexOf("GET /C15/go") >= 0)
+            {
+              Serial.println("C15 Go");
+              C15();
+              currentScene = "C15";
+            }
+            if (header.indexOf("GET /C16/go") >= 0)
+            {
+              Serial.println("C16 Go");
+              C16();
+              currentScene = "C16";
+            }
+            if (header.indexOf("GET /C17/go") >= 0)
+            {
+              Serial.println("C17 Go");
+              C17();
+              currentScene = "C17";
+            }
+            if (header.indexOf("GET /C18/go") >= 0)
+            {
+              Serial.println("C18 Go");
+              C18();
+              currentScene = "C18";
+            }
+            if (header.indexOf("GET /C19/go") >= 0)
+            {
+              Serial.println("C19 Go");
+              C19();
+              currentScene = "C19";
+            }
+            if (header.indexOf("GET /C19/go") >= 0)
+            {
+              Serial.println("C19 Go");
+              C19();
+              currentScene = "C19";
+            }
+            if (header.indexOf("GET /C20/go") >= 0)
+            {
+              Serial.println("C20 Go");
+              C20();
+              currentScene = "C20";
+            }
+
+
+
+            if (header.indexOf("GET /forward") >= 0)
+            {
+              Serial.println("forward");
+              forward();
+            }
+            if (header.indexOf("GET /backward") >= 0)
+            {
+              Serial.println("backward");
+              backward();
+            }
+            if (header.indexOf("GET /stop") >= 0)
+            {
+              Serial.println("stop");
+              stop();
+              currentScene = "Stop";
             }
 
             // Display the HTML web page
@@ -144,11 +400,28 @@ void loop()
             client.println("<p><a href=\"/C1/go\"><button class=\"button\">C1</button></a></p>");
             client.println("<p><a href=\"/C2/go\"><button class=\"button\">C2</button></a></p>");
             client.println("<p><a href=\"/C3/go\"><button class=\"button\">C3</button></a></p>");
-            // if (currentScene=="0") {
+            client.println("<p><a href=\"/C4/go\"><button class=\"button\">C4</button></a></p>");
+            client.println("<p><a href=\"/C5/go\"><button class=\"button\">C5</button></a></p>");
+            client.println("<p><a href=\"/C6/go\"><button class=\"button\">C6</button></a></p>");
+            client.println("<p><a href=\"/C7/go\"><button class=\"button\">C7</button></a></p>");
+            client.println("<p><a href=\"/C8/go\"><button class=\"button\">C8</button></a></p>");
+            client.println("<p><a href=\"/C85/go\"><button class=\"button\">C85</button></a></p>");
+            client.println("<p><a href=\"/C9/go\"><button class=\"button\">C9</button></a></p>");
+            client.println("<p><a href=\"/C10/go\"><button class=\"button\">C10</button></a></p>");
+            client.println("<p><a href=\"/C11/go\"><button class=\"button\">C11</button></a></p>");
+            client.println("<p><a href=\"/C12/go\"><button class=\"button\">C12</button></a></p>");
+            client.println("<p><a href=\"/C13/go\"><button class=\"button\">C13</button></a></p>");
+            client.println("<p><a href=\"/C14/go\"><button class=\"button\">C14</button></a></p>");
+            client.println("<p><a href=\"/C15/go\"><button class=\"button\">C15</button></a></p>");
+            client.println("<p><a href=\"/C16/go\"><button class=\"button\">C16</button></a></p>");
+            client.println("<p><a href=\"/C17/go\"><button class=\"button\">C17</button></a></p>");
+            client.println("<p><a href=\"/C18/go\"><button class=\"button\">C18</button></a></p>");
+            client.println("<p><a href=\"/C19/go\"><button class=\"button\">C19</button></a></p>");
+            client.println("<p><a href=\"/C20/go\"><button class=\"button\">C20</button></a></p>");
 
-            // } else {
-            //   //client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
-            // }
+            client.println("<p><a href=\"/forward\"><button class=\"button\">Forward 50</button></a></p>");
+            client.println("<p><a href=\"/backward\"><button class=\"button\">Backward 50</button></a></p>");
+            client.println("<p><a href=\"/stop\"><button class=\"button\">Stop</button></a></p>");
 
             client.println("</body></html>");
 
